994f0c84586cd7bec9e7854775d16f786f3c49ab
vagueunderstanding <87196018+vagueunderstanding@users.noreply.github.com>
2024-05-08 19:42:48 +0200

    Additional test for postgresql repo

--- tests/OverlyComplicatedBowling.Infrastructure.Tests/Repositories/PostgreSQLRepositoryTests.cs
+++ tests/OverlyComplicatedBowling.Infrastructure.Tests/Repositories/PostgreSQLRepositoryTests.cs
@@ -15,6 +15,25 @@ namespace OverlyComplicatedBowling.Infrastructure.Tests.Repositories
             var dbContextOptions = new DbContextOptionsBuilder<PostgreSQLDbContext>()
                 .UseInMemoryDatabase("mockdb")
                 .Options;
+            var dbContext = new PostgreSQLDbContext(dbContextOptions);
+            var repository = new PostgreSQLRepository(dbContext);
+            var game = Game.Start();
+
+            //Act
+            await repository.SaveGameAsync(game);
+            var result = await repository.LoadGameAsync(game.Id);
+
+            //Assert
+            result.Should().NotBeNull();
+        }
+
+        [TestMethod]
+        public async Task SaveGameAsync_SavesFrames()
+        {
+            //Arrange
+            var dbContextOptions = new DbContextOptionsBuilder<PostgreSQLDbContext>()
+                .UseInMemoryDatabase("mockdb")
+                .Options;
             var dbContextMock = new PostgreSQLDbContext(dbContextOptions);
             var repository = new PostgreSQLRepository(dbContextMock);
             var game = Game.Start();
@@ -25,6 +44,11 @@ namespace OverlyComplicatedBowling.Infrastructure.Tests.Repositories
 
             //Assert
             result.Should().NotBeNull();
+            result.Frames.Should().NotBeNull();
+            result.Frames.First().Key.Should().Be(1);
+            result.Frames.First().Value.GetType().Should().Be(typeof(NormalFrame));
+            result.Frames.Last().Key.Should().Be(10);
+            result.Frames.Last().Value.GetType().Should().Be(typeof(FinalFrame));
         }
     }
 }
