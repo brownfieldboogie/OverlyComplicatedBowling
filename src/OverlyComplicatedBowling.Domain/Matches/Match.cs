2c370e9764aaaac0a6ec8fa32d5b470ee6c07ccf
vagueunderstanding <87196018+vagueunderstanding@users.noreply.github.com>
2025-03-04 20:31:57 +0100

    Add initial Match domain model

---
+++ src/OverlyComplicatedBowling.Domain/Matches/Match.cs
@@ -0,0 +1,29 @@
+﻿using OverlyComplicatedBowling.Domain.Games;
+using System.Collections.ObjectModel;
+
+namespace OverlyComplicatedBowling.Domain.Matches
+{
+	public class Match
+	{
+		public Guid Id { get; private set; } = Guid.NewGuid();
+		internal SortedDictionary<int, Game> _games = [];
+		internal int _keyOfActiveGame = 0;
+		public IReadOnlyDictionary<int, Game> Games => new ReadOnlyDictionary<int, Game>(_games);
+
+		public static Match Start(int numberOfPlayers)
+		{
+			var match = new Match();
+			match.CreateGames(numberOfPlayers);
+			return match;
+		}
+
+		private void CreateGames(int numberOfPlayers)
+		{
+			for (int i = 0; i < numberOfPlayers; i++)
+			{
+				Game game = Game.Start();
+				_games.Add(i, game);
+			}
+		}
+	}
+}
---
+++ tests/OverlyComplicatedBowling.Domain.Tests/Matches/MatchTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using OverlyComplicatedBowling.Domain.Matches;
+
+namespace OverlyComplicatedBowling.Domain.Tests.Matches
+{
+	[TestClass]
+	public class MatchTests
+	{
+		[TestMethod]
+		public void Match_Start_CreatesMatchWithMultipleGames()
+		{
+			//Arrange
+			var numberOfPlayers = 2;
+
+			//Act
+			var match = Match.Start(numberOfPlayers);
+
+			//Assert
+			match.Games.Should().HaveCount(numberOfPlayers);
+			match.Games.Values.Select(g => g.Id).Should().OnlyHaveUniqueItems();
+		}
+	}
+}
